771db86d44d521bbc7973f38e8a10f756003e5fd
LeyvinoBezerra <leyvinobezerra.develop@gmail.com>
2022-02-15 10:56:05 -0300

    avançando

---
+++ src/Lista1Pacote/Q10.java
@@ -0,0 +1,20 @@
+package Lista1Pacote;
+
+import java.util.Scanner;
+
+public class Q10 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+
+		System.out.println("Digite um Numero Inteiro:\n");
+		int num = ler.nextInt();
+
+		if (num % 2 == 0) {
+			System.out.println("O Numero é Par!" + " " + num);
+		} else {
+			System.out.println("O Numero é Impar!" + " " + num);
+		}
+	}
+
+}
