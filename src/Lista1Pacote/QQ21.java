4b17eb53283038cb4f16b310a614731b46c5c024
LeyvinoBezerra <leyvinobezerra.develop@gmail.com>
2022-02-16 20:00:20 -0300

    next

---
+++ src/Lista1Pacote/QQ21.java
@@ -0,0 +1,19 @@
+package Lista1Pacote;
+
+import java.util.Scanner;
+
+public class QQ21 {
+
+	 public static void main( String[] args ) {
+		      int c = 0;
+		      Scanner ler = new Scanner( System.in );
+		      System.out.println( "Digite os números: ");
+		      int a = ler.nextInt();
+		      int b = ler.nextInt();
+		      c = a + b;
+		      System.out.println( " A soma de A e B É: " + c );
+		   }
+		 
+		
+
+}
